4fc5ff06f047cacc00ddd41ddee042ed62c3dc43
Matt Evans <matt@ozlabs.org>
2021-09-07 17:55:40 +0100

    test_conns: Show GPIO state
    Prints GPIO values and logs which pins have been seen to change.
    Useful for a connectivity test when soldering.

--- test_conns.c
+++ test_conns.c
@@ -1,18 +1,65 @@
+/* test_conns
+ *
+ * A super-simple connection tester:  puts all GPIO into input, and prints which pins change
+ * due to external (likely human-led) probing.
+ *
+ * Hit 'r' to reset sticky "have seen a change" state.
+ */
+
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+
+static void dump_state(uint32_t now, uint32_t changed)
+{
+	// Row of bit numbers
+	printf("     ");
+	for (int w = 0; w < 30; w++) {
+		printf("%d ", w / 10);
+	}
+	printf("\n     ");
+	for (int w = 0; w < 30; w++) {
+		printf("%d ", w % 10);
+	}
+	printf("\nNow: ");
+	for (int w = 0; w < 30; w++) {
+		printf("%d ", !!(now & (1 << w)));
+	}
+	printf("\nCh:  ");
+	for (int w = 0; w < 30; w++) {
+		printf("%d ", !!(changed & (1 << w)));
+	}
+	printf("\n\n");
+}
 
 int main()
 {
 	stdio_init_all();
-	for (int i = 0; i < 10; i++) {
-		printf("Hello, world!\n");
-		sleep_ms(1000);
-	}
-	printf("Now, hit a key plz:\n");
-	while (true) {
-		char c = getchar();
 
-		printf("Woo u hits %d\n", c);
+	sleep_ms(1000);
+	printf("Connection tester, ohai.\n");
+
+	// All GPIOs input with a weak pullup:
+	for (int g = 0; g < 30; g++) {
+		gpio_set_function(g, GPIO_FUNC_NULL);
+		gpio_pull_up(g);
+		gpio_set_input_enabled(g, true);
 	}
+	sleep_ms(1000);
+
+	uint32_t initial_gpio = gpio_get_all();
+	uint32_t cumulative = initial_gpio;
+	uint32_t changed = 0;
+
+	while (true) {
+		uint32_t curr = gpio_get_all();
+
+		changed |= (curr ^ initial_gpio);
+
+		dump_state(curr, changed);
+		sleep_ms(100);
+	}
+
 	return 0;
 }
