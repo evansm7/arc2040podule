b9d0e5477c200c7739546f88a5dd643a1181f654
Matt Evans <matt@ozlabs.org>
2021-09-22 14:11:37 +0100

    podule i/f: Clamp addresses, robustness, debug, and bugfix
    Only D6:0 were initialised... D7 was folorn :(  Fixed, now can reliably read 8b data!

--- podule_interface.c
+++ podule_interface.c
@@ -69,6 +69,11 @@ static bool     is_write(uint32_t i)
         return (i & ((1 << GPIO_NSEL) | (1 << GPIO_NWR))) == 0;
 }
 
+static bool     is_selected(uint32_t i)
+{
+        return (i & (1 << GPIO_NSEL)) == 0;
+}
+
 static void     data_inputs(void)
 {
         gpio_set_dir_in_masked(0xff << GPIO_D0);
@@ -79,7 +84,8 @@ static void     data_outputs(void)
         gpio_set_dir_out_masked(0xff << GPIO_D0);
 }
 
-volatile uint32_t       dbg_info1;
+volatile uint32_t       dbg_info1 = 0;
+volatile uint32_t       dbg_trx_count = 0;
 
 static void podule_if_thread(void)
 {
@@ -100,7 +106,7 @@ static void podule_if_thread(void)
 
                 if (is_read(io)) {
                         uint32_t addr = get_addr(io);
-                        uint32_t data = podule_space[addr];
+                        uint32_t data = podule_space[addr & 0xfff];
 
                         gpio_put_masked(0xff << GPIO_D0,
                                         data << GPIO_D0);
@@ -114,6 +120,7 @@ static void podule_if_thread(void)
                         } while(is_read(io));
 
                         data_inputs();
+                        dbg_trx_count++;
 
                 } else if (is_write(io)) {
                         uint32_t addr = get_addr(io);
@@ -124,22 +131,23 @@ static void podule_if_thread(void)
 
                         dbg_info1 = 0xc0000000 | addr | (data << 16);
 
-                        if (addr >= 2048) {
+                        if (addr >= 2048 && addr < 4096) {
                                 podule_space[addr] = data;
                         }
                         // Wait for write cycle to finish:
                         do {
                                 io = gpio_get_all();
                         } while(is_write(io));
+
+                        dbg_trx_count++;
                 }
         }
 }
 
 void	podule_if_init(void)
 {
-
         // Address/control inputs:
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 8; i++) {
                 CFG_INPUT(GPIO_D0 + i);         // D0-D7
                 gpio_set_drive_strength(GPIO_D0 + i, GPIO_DRIVE_STRENGTH_8MA);
         }
@@ -168,11 +176,11 @@ void	podule_if_debug(void)
 {
         uint32_t i = gpio_get_all();
 
-        printf("A = %04x, D[7:0] = %02x, NSEL%d, NRD%d, NWR%d, NRST%d, %08x\n",
+        printf("A = %04x, D[7:0] = %02x, NSEL%d, NRD%d, NWR%d, NRST%d, %08x, %d\n",
                get_addr(i), get_data(i),
                gpio_get(GPIO_NSEL),
                gpio_get(GPIO_NRD),
                gpio_get(GPIO_NWR),
                gpio_get(GPIO_NRST_I),
-               dbg_info1);
+               dbg_info1, dbg_trx_count);
 }
