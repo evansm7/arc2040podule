eb46faa0925d4299ece36f2b4ed43bc2acbc2e3c
Matt Evans <matt@ozlabs.org>
2021-09-24 17:43:24 +0100

    podule_interface: Rewrite in assembler
    Timing is tight, particularly for sync accesses.  This rewrite saves a ton of cycles, and
    makes host access reliable finally.

--- podule_interface.c
+++ podule_interface.c
@@ -49,6 +49,8 @@ volatile uint8_t podule_space[4096];
                 gpio_set_dir(x, GPIO_OUT);              \
         } while(0)
 
+
+#ifdef DEBUG
 static uint32_t get_addr(uint32_t i)
 {
         uint32_t j = i & (7 << GPIO_A2);
@@ -61,101 +63,187 @@ static uint8_t  get_data(uint32_t i)
         return (i & (0xff << GPIO_D0)) >> (GPIO_D0);
 }
 
-static bool     is_read(uint32_t i)
-{
-        return (i & ((1 << GPIO_NSEL) | (1 << GPIO_NRD))) == 0;
-}
+struct _debug {
+        volatile uint32_t       rd_addr;
+        volatile uint32_t       rd_data;
+        volatile uint32_t       rd_count;
+        volatile uint32_t       wr_addr;
+        volatile uint32_t       wr_data;
+        volatile uint32_t       wr_count;
+} debug;
+#endif
 
-static bool     is_write(uint32_t i)
+static void __no_inline_not_in_flash_func(podule_if_thread)(void)
 {
-        return (i & ((1 << GPIO_NSEL) | (1 << GPIO_NWR))) == 0;
-}
+        /* Simple bus interface operates as follows:
+         *
+         * Idle:        Wait for (/RD or /WR) and /SEL
+         *              Go to do_read or do_write
+         *
+         * do_read:     Acquire byte, set D as outputs, apply output data,
+         *              wait for /RD to go inactive. (Ideally, and /SEL)
+         *              D set as inputs.  Return to Idle.
+         *
+         * do_write:    Sample D inputs, store byte.  Wait for /WR (and /SEL)
+         *              to go inactive.  Return to idle.
+         *
+         * Synchronous cycles happen (e.g. PI/ECId); there's < 200ns between
+         * /RD |_ and data needing to be ready (>=50ns setup before _|).
+         *
+         * This thread runs from RAM (avoiding XIP/cache unpredictability), and
+         * with IRQs off.
+         */
 
-static bool     is_selected(uint32_t i)
-{
-        return (i & (1 << GPIO_NSEL)) == 0;
-}
-
-static void     data_inputs(void)
-{
-        gpio_set_dir_in_masked(0xff << GPIO_D0);
-}
-
-static void     data_outputs(void)
-{
-        gpio_set_dir_out_masked(0xff << GPIO_D0);
-}
-
-volatile uint32_t       dbg_rd_addr = 0;
-volatile uint32_t       dbg_rd_data = 0;
-volatile uint32_t       dbg_rd_count = 0;
-volatile uint32_t       dbg_wr_addr = 0;
-volatile uint32_t       dbg_wr_data = 0;
-volatile uint32_t       dbg_wr_count = 0;
-
-static void podule_if_thread(void)
-{
         save_and_disable_interrupts();
         gpio_clr_mask(0xff << GPIO_D0);
+
+        /* Asm is tuned for specific pins, assert this: */
+#if GPIO_D0 != 0
+#error "D0 not at 0"
+#endif
         while (1) {
-                /* Simple bus interface operates as follows:
-                 *
-                 * Idle:        Wait for (/RD or /WR) and /SEL
-                 *              Go to do_read or do_write
-                 *
-                 * do_read:     Acquire byte, set D as outputs, apply output data,
-                 *              wait for /RD to go inactive. (Ideally, and /SEL)
-                 *              D set as inputs.  Return to Idle.
-                 *
-                 * do_write:    Sample D inputs, store byte.  Wait for /WR (and /SEL)
-                 *              to go inactive.  Return to idle.
-                 *
-                 * Synchronous cycles happen (e.g. PI/ECId); there's < 200ns between
-                 * /RD |_ and data needing to be ready (>=50ns setup before _|).
-                 *
-                 * Looking at compiler output, there are a couple of things that could
-                 * be improved using asm; this works but I'd like more margin.
+                /* Don't ask.  Though there's a loop in the asm (and
+                 * this while shouldn't be necessary), I saw some reg
+                 * allocation weirdness when this wasn't in a while
+                 * block of its own.
                  */
-                uint32_t io = gpio_get_all();
+                uint32_t unused1, unused2, unused3, unused4;
+                asm __volatile__(
+                        "pif_mainloop:                  \n"
+                        "mov    %1, %[rd_sel_mask]      \n"
+                        "mov    %2, %[wr_sel_mask]      \n"
+                        "pif_wait_strobe:               \n"
+                        "ldr    %0, [%[io], %[in]]      \n"
+                        // Selected for read or write?
+                        "tst    %0, %1                  \n"
+                        "beq    pif_rd                  \n"
+                        "tst    %0, %2                  \n"
+                        "beq    pif_wr                  \n"
+                        "b      pif_wait_strobe         \n"
 
-                if (is_read(io)) {
-                        uint32_t addr = get_addr(io);
-                        uint32_t data = podule_space[addr & 0xfff];
+                        "pif_rd:                        \n"
+                        // Construct address (make these pins contiguous on V2!):
+                        "mov    %2, %[addr_mask]        \n"
+                        // Addr bits from input pins:
+                        "and    %2, %2, %0              \n"
+                        // Lower bits A[4:2]:
+                        "mov    %1, %[addrl_mask]       \n"
+                        "and    %1, %1, %0              \n"
+                        // Trick: Shift A[4:2] field right by adding to self:
+                        "add    %1, %1, %2              \n"
+                        "asr    %1, %1, %[shr_a2]       \n"
+                        // %1 now contains the address as A[11:0].  Get read data:
+                        "ldrb   %0, [%[ps], %1]         \n"
+                        // Set GPIO data output value:
+                        "str    %0, [%[io], %[setmask]] \n"
+                        // Set GPIO pins output:
+                        "mov    %2, #0xff               \n" // D[7:0] mask
+                        "str    %2, [%[io], %[oe_set]]  \n"
 
-                        gpio_set_mask(data << GPIO_D0); // Faster than gpio_put_masked()
-                        data_outputs();
 #ifdef DEBUG
-                        dbg_rd_addr = addr;
-                        dbg_rd_data = data;
-                        dbg_rd_count++;
+                        // Debug:
+                        "str    %1, [%[debug], %[dbg_r_addr]] \n" // Addr
+                        "str    %0, [%[debug], %[dbg_r_data]] \n" // Data
+                        "ldr    %0, [%[debug], %[dbg_r_count]] \n"
+                        "add    %0, %0, #1              \n"
+                        "str    %0, [%[debug], #8]      \n"
 #endif
+                        // Whew.  Now wait for SEL to go high:\n"
+                        "mov    %1, %[sel_mask]         \n"
+                        "pif_rd_done_wait:                  \n"
+                        "ldr    %0, [%[io], %[in]]      \n"
+                        "tst    %0, %1                  \n"
+                        "beq    pif_rd_done_wait        \n"
+                        // Finally, clear D, set pins inputs:
+                        "str    %2, [%[io], %[oe_clr]]  \n"
+                        "str    %2, [%[io], %[clrmask]] \n"
+                        // Done!
+                        "b      pif_mainloop            \n"
 
-                        // Wait for read cycle to finish:
-                        do {
-                                io = gpio_get_all();
-                        } while(is_selected(io)); // Hold after /RD rises
+                        "pif_wr:                        \n"
+                        // Construct address, as above:
+                        "mov    %2, %[addr_mask]        \n"
+                        "and    %2, %2, %0              \n"
+                        "mov    %1, %[addrl_mask]       \n"
+                        "and    %1, %1, %0              \n"
+                        "add    %1, %1, %2              \n"
+                        "asr    %1, %1, %[shr_a2]       \n"
+                        // Extract data (valid at /WR falling edge):
+                        "mov    %2, #0xff               \n" // D[7:0] mask
+                        "and    %0, %0, %2              \n"
+                        // Is addr >=2048? (0-2047 are R/O)
+                        "mov    %2, #0x8                \n"
+                        "lsl    %2, %2, #8              \n"
+                        "cmp    %1, %2                  \n"
+                        "blt    pif_wr_done             \n"
+                        "strb   %0, [%[ps], %1]         \n"
 
-                        data_inputs();
-                        gpio_clr_mask(0xff << GPIO_D0);
-                } else if (is_write(io)) {
-                        uint32_t addr = get_addr(io);
-                        uint32_t data;
-
-                        io = gpio_get_all();    // resample WR data (wait a bit?)
-                        data = get_data(io);
+                        "pif_wr_done:                   \n"
 #ifdef DEBUG
-                        dbg_wr_addr = addr;
-                        dbg_wr_data = data;
-                        dbg_wr_count++;
+                        // Debug:
+                        "str    %1, [%[debug], %[dbg_w_addr]] \n"
+                        "str    %0, [%[debug], %[dbg_w_data]] \n"
+                        "ldr    %0, [%[debug], %[dbg_w_count]] \n"
+                        "add    %0, %0, #1              \n"
+                        "str    %0, [%[debug], %[dbg_w_count]] \n"
 #endif
-                        if (addr >= 2048 && addr < 4096) {
-                                podule_space[addr] = data;
-                        }
-                        // Wait for write cycle to finish:
-                        do {
-                                io = gpio_get_all();
-                        } while(is_write(io));
-                }
+                        // Whew.  Now wait for SEL to go high:\n"
+                        "mov    %1, %[sel_mask]         \n"
+                        "pif_wr_done_wait:              \n"
+                        "ldr    %0, [%[io], %[in]]      \n"
+                        "tst    %0, %1                  \n"
+                        "beq    pif_wr_done_wait        \n"
+                        // Done!
+                        "b      pif_mainloop            \n"
+
+                        /* Constraints follow.
+                         * Note, for thumb, h = high regs, l = low regs; LDR/STR
+                         * can only use low regs.
+                         */
+
+                        /* Outputs: these are used as temporaries, %0-%3. */
+                        :
+                        "=l"(unused1), "=l"(unused2), "=l"(unused3), "=l"(unused4)
+
+                        /* Inputs: */
+                        :
+
+                        /* Pointer to shared memory: */
+                        [ps]"l"(podule_space),
+
+                        /* GPIO registers: */
+                        [io]"l"(sio_hw),
+
+                        /* One 'l' register free. */
+
+                        /* Various constants/masks to load into (hi) regs: */
+                        [rd_sel_mask]"h"( (1 << GPIO_NSEL) | (1 << GPIO_NRD) ),
+                        [wr_sel_mask]"h"( (1 << GPIO_NSEL) | (1 << GPIO_NWR) ),
+                        [sel_mask]"h"( 1 << GPIO_NSEL ),
+                        [addrl_mask]"h"( (7 << GPIO_A2) ),
+                        [addr_mask]"h"( (0x1ff << GPIO_A5) | (7 << GPIO_A2) ),
+
+                        /* Offsets of GPIO registers: */
+                        [in]"i"(offsetof(sio_hw_t, gpio_in)),
+                        [setmask]"i"(offsetof(sio_hw_t, gpio_set)),
+                        [clrmask]"i"(offsetof(sio_hw_t, gpio_clr)),
+                        [oe_set]"i"(offsetof(sio_hw_t, gpio_oe_set)),
+                        [oe_clr]"i"(offsetof(sio_hw_t, gpio_oe_clr)),
+#ifdef DEBUG
+                        /* Debug counters: */
+                        [debug]"l"(&debug),
+
+                        /* Debug struct offsets: */
+                        [dbg_r_addr]"i"(offsetof(struct _debug, rd_addr)),
+                        [dbg_r_data]"i"(offsetof(struct _debug, rd_data)),
+                        [dbg_r_count]"i"(offsetof(struct _debug, rd_count)),
+                        [dbg_w_addr]"i"(offsetof(struct _debug, wr_addr)),
+                        [dbg_w_data]"i"(offsetof(struct _debug, wr_data)),
+                        [dbg_w_count]"i"(offsetof(struct _debug, wr_count)),
+#endif
+                        /* Misc constants: */
+                        [shr_a2]"i"(GPIO_A2 + 1)
+                        );
         }
 }
 
@@ -184,20 +272,31 @@ void	podule_if_init(void)
         gpio_put(GPIO_HRST, false);             // Don't reset...
         CFG_OUTPUT(GPIO_HRST);
 
+#ifdef DEBUG
+        debug.rd_addr = 0;
+        debug.rd_data = 0;
+        debug.rd_count = 0;
+        debug.wr_addr = 0;
+        debug.wr_data = 0;
+        debug.wr_count = 0;
+#endif
+
         multicore_launch_core1(podule_if_thread);
 }
 
 void	podule_if_debug(void)
 {
+#ifdef DEBUG
         uint32_t i = gpio_get_all();
 
-        printf("A %04x, D %02x, NSEL%d, NRD%d, NWR%d, NRST%d; R %04x, %02x, %d; W %04x, %02x, %d\n",
+        printf("A:%04x, D:%02x, NSEL%d, NRD%d, NWR%d, NRST%d; R %04x, %02x, %d; W %04x, %02x, %d\n",
                get_addr(i), get_data(i),
                gpio_get(GPIO_NSEL),
                gpio_get(GPIO_NRD),
                gpio_get(GPIO_NWR),
                gpio_get(GPIO_NRST_I),
-               dbg_rd_addr, dbg_rd_data, dbg_rd_count,
-               dbg_wr_addr, dbg_wr_data, dbg_wr_count
+               debug.rd_addr, debug.rd_data, debug.rd_count,
+               debug.wr_addr, debug.wr_data, debug.wr_count
                 );
+#endif
 }
