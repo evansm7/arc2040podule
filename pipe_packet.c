05a69f75d8da8aa272bb30660d4e69518abd0be7
Matt Evans <matt@ozlabs.org>
2021-09-28 15:59:24 +0100

    pipe_packet: Implement packet receive path
    Takes a shortcut in that it doesn't allocate arbitrary RX buffers & expects to reuse
    the entire RX_BUFF space, but works.
    
    Implements basic backpressure; the server will see a failure to write if the Arc doesn't
    consume data.  No data is lost/link is reliable.

--- pipe_packet.c
+++ pipe_packet.c
@@ -25,11 +25,16 @@ typedef struct {
         unsigned int tx_pos;
         uint8_t tx_buf[512 + 3];
 
-        bool rx_ongoing;
+        unsigned int rx_total;
+        unsigned int rx_pos;
+        unsigned int rx_last_descr;
+        bool rx_packet_pending;
+        uint8_t rx_buf[512 + 3];
 } pp_state_t;
 
 static pp_state_t state;
-static uint8_t rx_buf[1024];
+
+#define PKT_HDR_SIZE    3
 
 
 // Called at init, but can also be requested by loader on soft reset:
@@ -44,8 +49,11 @@ void    pipe_init(void)
 
         // Reset state
         state.tx_ongoing = false;
-        state.rx_ongoing = false;
+        state.rx_pos = 0;
         state.tx_pos = 0;
+
+        state.rx_last_descr = 0;
+        state.rx_packet_pending = false;
 }
 
 static void     pipe_tx_done(void)
@@ -98,7 +106,7 @@ static void     pipe_tx_start(uint32_t descr)
         state.tx_buf[1] = len & 0xff;
         state.tx_buf[2] = (len >> 8) & 0xff;
 
-        memcpy(&state.tx_buf[3], tx_data, len);
+        memcpy(&state.tx_buf[PKT_HDR_SIZE], tx_data, len);
         state.tx_total = len + 3;
 
         /* Try to queue as much as possible in the USB TX FIFOs: */
@@ -178,22 +186,158 @@ static void     pipe_tx_continue(void)
         }
 }
 
-static void dump_regs()
+
+/* Add packet to RX buffer.  Returns true if accepted/there is space,
+ * allowing rudimentary flow-control/backpressure.
+ */
+static bool     pipe_rx_packet(uint8_t cid, uint16_t len, uint8_t *data)
 {
         volatile uint8_t *r = podule_if_get_regs();
 
-        for (int i = 0; i < 2048; i += 16) {
-                printf("%03x: ", i);
-                for (int j = 0; j < 16; j++) {
-                        printf("%02x ", r[i+j]);
+        // FIXME: Multiple RX buffers!
+        unsigned int last = state.rx_last_descr;
+        unsigned int head = r[PR_RX_HEAD];
+
+        /* Before then, this is a hack.  It makes sure there are no
+         * unconsumed RX descriptors (checking that the last created
+         * has been consumed), because an unconsumed descriptor means
+         * an unconsumed RX buffer (and there's only one of those).
+         */
+
+        if (PR_DESCR_IS_READY(PR_RX_DESCR(r, last))) {
+                static int last_last = -1;
+                if (last_last != last) {
+                        // Dumb rate-limiting
+                        printf("[pipe RX: No room for packet]\n");
+                        last_last = last;
                 }
-                printf("  ");
-                for (int j = 0; j < 16; j++) {
-                        uint8_t c = r[i+j];
-                        printf("%c", c >= 32 ? c : '.');
-                }
-                printf("\n");
+                return false;
         }
+
+        if (len > PR_RX_TX_BUFSZ) {
+                printf("[pipe RX ERROR: Packet size %d is too large! Dropping.]\n",
+                       len);
+                return true;
+        }
+
+        /* Create a descriptor at PR_RX_HEAD: */
+        unsigned int addr = 0;  // Right at the start of the RX buffer
+
+        memcpy((void *)&r[PR_RX_BUFFERS + addr], data, len);
+        // Once data is in place, construct the descriptor -- making it ready:
+        PR_RX_DESCR(r, head) = ((uint32_t)cid << PR_DESCR_CID_SHIFT) |
+                ((uint32_t)len << PR_DESCR_SIZE_SHIFT) |
+                (addr << PR_DESCR_ADDR_SHIFT) |
+                PR_DESCR_READY;
+
+        state.rx_last_descr = head;
+        // Move on head pointer:
+        head = (head + 1) & 7;
+        r[PR_RX_HEAD] = head;
+
+        return true;
+}
+
+/* Assembles a single packet from possibly multi-chunk multi-receives,
+ * staging the data into the state.rx_buf buffer until it's complete, then
+ * copying that into the RX buffer area.
+ *
+ * This could be made more complex and zero-copy by receiving just the header
+ * (to work out size) then allocating a correctly-sized block in the RX area,
+ * but "meh" and we're not close to needing to scrimp on memory either.
+ */
+static void     pipe_rx(void)
+{
+        unsigned int len = 0;
+        static unsigned int pkt_counter = 0;
+
+        /* If a packet's pending, try to deliver it without receiving more USB
+         * data.
+         */
+        if (!state.rx_packet_pending) {
+                /* If we're in the middle of a packet, state.rx_pos is
+                 * non-zero so that this receive places data at the end of
+                 * previous data: */
+                len = tud_cdc_n_read(0, &state.rx_buf[state.rx_pos],
+                                       sizeof(state.rx_buf) - state.rx_pos);
+#if DEBUG > 0
+                printf("[pipe RX %d bytes]\n", len);
+#endif
+
+                state.rx_pos += len;
+        }
+
+        // Check for packet partial/complete:
+        if (state.rx_pos >= PKT_HDR_SIZE) {
+                // Decode size
+                uint16_t data_len = state.rx_buf[1] | ((uint32_t)state.rx_buf[2] << 8);
+                state.rx_total = PKT_HDR_SIZE + data_len;
+#if DEBUG > 2
+                printf("[pipe RX packet header: CID%d, size %d]\n",
+                       state.rx_buf[0], state.rx_total);
+#endif
+                // Did we get all of it?
+                if (state.rx_pos >= state.rx_total) {
+                        // Pop it into the RX buffer/descriptor for Arc to see:
+                        bool accepted = pipe_rx_packet(state.rx_buf[0],
+                                                       data_len,
+                                                       &state.rx_buf[PKT_HDR_SIZE]);
+#if DEBUG > 1
+                        if (accepted) {
+                                printf("[pipe RX packet complete: CID%d, size %d]\n",
+                                       state.rx_buf[0], state.rx_total);
+                        } else {
+                                // Rate-limit this!
+                                static int last_count = ~0;
+                                if (pkt_counter != last_count) {
+                                        printf("[pipe RX packet stalled: "
+                                               "CID%d, size %d]\n",
+                                               state.rx_buf[0], state.rx_total);
+                                        last_count = pkt_counter;
+                                }
+                        }
+#endif
+                        /* If it wasn't accepted (no room in RX queue,
+                         * Arc isn't listening) then just quit now.
+                         * It's safe, because next loop we'll try this
+                         * all over again.  In the meantime, USB reads
+                         * aren't performed so the backpressure
+                         * propagates to the host.
+                         */
+                        if (!accepted) {
+                                state.rx_packet_pending = true;
+                                return;
+                        } else {
+                                pkt_counter++;
+                                state.rx_packet_pending = false;
+                        }
+
+                        // Did we read too much/some of the next packet?
+                        if (state.rx_pos > state.rx_total) {
+                                /* Move the excess data read to the
+                                 * bottom of the buffer, so that the
+                                 * next read just tacks data onto the
+                                 * end:
+                                 */
+                                unsigned int excess = state.rx_total -
+                                        state.rx_pos;
+                                memmove(&state.rx_buf[0],
+                                        &state.rx_buf[state.rx_total],
+                                        excess);
+                                state.rx_pos = excess;
+#if DEBUG > 1
+                                printf("[pipe RX packet excess %d]\n",
+                                       excess);
+#endif
+                        } else {
+                                /* We read an exact amount,  Complete now,
+                                 * and next RX occurs at the start, afresh.
+                                 */
+                                state.rx_pos = 0;
+                        }
+                }
+        }
+        // Else do nothing, next reception we'll check again.
 }
 
 // Check whether the packet descriptors have some work for us (or an ongoing transfer)
@@ -214,22 +358,8 @@ void pipe_poll(void)
         //////////////////////////////////////////////////////////////////////
         // Receive
 
-        /* FIXME: Simple data sink/debugging for now.  Implement RX!
-         */
-        if (cdc_connected && tud_cdc_n_available(0)) {
-                unsigned int count = tud_cdc_n_read(0, rx_buf, sizeof(rx_buf));
-#if DEBUG > 0
-                printf("+++ Rx'd %d bytes from USB\n", count);
-#endif
-#if DEBUG > 2
-                for (int i = 0; i < count; i++) {
-                        uint8_t c = rx_buf[i];
-                        printf("%02x(%c) ", c, c >= 32 ? c : '.');
-                }
-                printf("\r\n");
-#endif
-                if (rx_buf[0] == '^')
-                        dump_regs();
+        if ((cdc_connected && tud_cdc_n_available(0)) || state.rx_packet_pending) {
+                pipe_rx();
         }
 
         //////////////////////////////////////////////////////////////////////
